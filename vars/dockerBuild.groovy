708b6ad34d5e7b0e6c899da096554384450bd108
arupsscet1-buff <arupsscet1@gmail.com>
2026-07-19 00:22:54 +0530

    docker build library

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,12 @@
 def call(Map config = [:]) {
-    def port = config.get(port: '')
-    def svc  = config.get(svc_name: '')
+    def port = config.get('port': '')
+    def svc  = config.get('svc_name': '')
 
-    sh 'docker build \
-        -f docker/Dockerfile \
-        --build-arg JAR_FILE=${svc}/target/${svc}*.jar \
-        --build-arg EXPOSED_PORT=${port} \
-        -t ${svc}:${BUILD_NUMBER} .'
+    sh """
+    docker build \
+    -f docker/Dockerfile \
+    --build-arg ARTIFACT_NAME=${svc}/target/${svc}*.jar \
+    --build-arg EXPOSED_PORT=${port} \
+    -t ${svc}:${BUILD_NUMBER} .
+    """
 }
