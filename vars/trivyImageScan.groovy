0048e8e5864c1edb7f0c673e446cc7b081be8863
arupsscet1-buff <arupsscet1@gmail.com>
2026-07-19 18:00:52 +0530

    modification

--- vars/trivyImageScan.groovy
+++ vars/trivyImageScan.groovy
@@ -1,13 +1,14 @@
 def call(Map config = [:]) {
 
     def svc  = config.get('svc_name', 'example-svc')
+    def exit_code = config.get('exit_code', '')
     sh """
     trivy image \
     --scanners vuln \
     --severity HIGH,CRITICAL \
     --ignore-unfixed \
     --format table \
-    --exit-code 1 \
+    ${exit_code} \
     --no-progress \
     ${svc}:${env.BUILD_NUMBER}
     """
