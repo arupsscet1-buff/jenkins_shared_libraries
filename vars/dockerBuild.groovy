5e0c9836ddf9aaa7cb7d1d07c254cb8298ea9f06
arupsscet1-buff <arupsscet1@gmail.com>
2026-07-24 01:13:22 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,17 +2,17 @@ def call(Map config = [:]) {
     def port = config.get('port', '8080')
     def svc  = config.get('svc_name', 'example-svc')
 
-   // def version = sh(
-     //   script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout',
-       // returnStdout: true
-    //).trim()
+   def version = sh(
+       script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout',
+       returnStdout: true
+   ).trim()
 
-    echo "Version: 4.0.1"
+    echo "Version: ${version}"
 
     sh """
     docker build \
     -f docker/Dockerfile \
-    --build-arg ARTIFACT_NAME=${svc}/target/${svc}-4.0.1 \
+    --build-arg ARTIFACT_NAME=${svc}/target/${svc}-${version} \
     --build-arg EXPOSED_PORT=${port} \
     -t ${svc}:${env.BUILD_NUMBER} .
     """
